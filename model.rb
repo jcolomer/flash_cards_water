07b03fdc5a27c972cf7abef3fe6f28710817bf99
Anders Howerton & Devon Bradley <pair+AndersEverett.dbradleyfl@devbootcamp.com>
2014-09-05 12:22:05 -0700

    working parse and question shuffle/pop

--- model.rb
+++ model.rb
@@ -1,4 +1,23 @@
-require 'pp'
+class Model
+  ALL_CARDS = Array.new
+
+  def self.parse
+    file = File.open("flashcard_samples.txt").readlines
+    until file.length == 0
+      one_card = file.shift(3)
+      definition = one_card[0].chop
+      answer = one_card[1].chop
+
+      ALL_CARDS << Card.new(definition,answer)
+    end
+  end
+
+  def self.pick_question
+    ALL_CARDS.shuffle!
+
+    ALL_CARDS.pop.definition
+  end
+end
 
 class Card
   attr_reader :definition, :answer
@@ -8,25 +27,7 @@ class Card
   end
 end
 
-ALL_CARDS = Array.new
-
-def parse
-  file = File.open("flashcard_samples.txt").readlines
-  until file.length == 0
-    one_card = file.shift(3)
-    definition = one_card[0].chop
-    answer = one_card[1].chop
-
-    ALL_CARDS << Card.new(definition,answer)
-  end
-end
-
-
-parse
-
-ALL_CARDS.each do |card|
-  puts card.definition
-  puts
-  puts card.answer
-  puts
-end
+#Driver Code
+Model.parse
+Model.pick_question
+Model.pick_question
