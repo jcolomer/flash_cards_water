eb0ad16a9444f109049be913c8cb8f27c37cb6c1
jcolomer <julioindevbootcamp@gmail.com>
2014-09-05 11:58:51 -0700

    Initial commit controller file

---
+++ controller.rb
@@ -0,0 +1,39 @@
+
+# Controller
+
+class Controller
+
+  def self.run
+    welcome_message
+
+    get_input_from_file
+
+    shuffle_objects_array
+
+    until user_input == "exit" || objects_array.empty?
+        play_next_round
+
+          get_next_card
+          display_definition_to_user
+          get_user_input
+          check_answer
+            if user_input == Card.solution
+              contratulations_message
+              play_next_round
+            else
+              try_again_message
+              get_user_input
+              check_answer
+            end
+
+     end
+
+    display_exit_message
+    exit!
+
+
+end
+
+
+Controller.run
+
