8621fc1d51f4b4425f278fbc75c1842375588f24
Anders Howerton & Devon Bradley <pair+AndersEverett.dbradleyfl@devbootcamp.com>
2014-09-05 17:28:23 -0700

    added sound to interface

--- view.rb
+++ view.rb
@@ -2,6 +2,9 @@ class View
 attr_accessor :user_input
   def self.welcome_message
     puts "Welcome to Flash Cards. Hit enter to start."
+    puts "__________________________________"
+
+    `say "Welcome to Flash Cards. Hit enter to start."`
     @user_input = gets.chomp
     if @user_input.length == 0
       puts "Getting your first question..."
@@ -21,11 +24,12 @@ attr_accessor :user_input
 
   def self.congratulations_message
     puts "Congratulations! You are correct."
-
+    `say "Yahhhh awesome epic gnarly nicely done"`
   end
 
   def self.try_again_message
     puts "Sorry, that was not correct. Please try again."
+    `say "Dude. Pay attention. Seriously this is ridiculous"`
   end
 
 
